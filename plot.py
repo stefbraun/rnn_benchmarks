5aaa60aa0416aa16c14b167ab8924be4b5d9bdd4
stefbraun <stefan.sb.braun@outlook.com>
2017-06-02 14:08:20 +0200

    Fixed 1x320GRU plot

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 from data import bar_chart
 import matplotlib.pyplot as plt
 
-fig,ax=bar_chart(title='4x320 bidir LSTM network with cross entropy loss', selection=[1,2,3])
+fig,ax=bar_chart(title='1x320 unidir GRU network with cross entropy loss', selection=[1,2,3])
 # fig.savefig("results/bars.pdf", bbox_inches='tight')
 fig.savefig("results/bars.png", bbox_inches='tight')
 plt.show()
